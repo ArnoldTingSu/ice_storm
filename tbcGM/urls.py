f2cf40791e1e2cee6f4b446a741990270e0f47f5
Arnold Su <arnoldtingsu@gmail.com>
2021-04-16 17:12:54 -0700

    first commit

---
+++ tbcGM/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home),
+    path('register', views.register),
+    path('guilds', views.guilds),
+    path('login', views.login),
+    path('guild/<int:guild_id>/roster', views.guild_roster),
+    path('guild/<int:guild_id>/edit', views.guild_roster_edit),
+    path('guild/<int:guild_id>/member/<int:player_id>', views.player_profile)
+
+]
--- tbcGM/views.py
+++ tbcGM/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
+
+def guilds(request):
+    return render(request, 'guilds.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def guild_roster(request, guild_id):
+    return render(request, 'guild_roster.html')
+
+def guild_roster_edit(request, guild_id):
+    return render(request, 'guild_roster_edit.html')
+
+def player_profile(request, guild_id, player_id):
+    return render(request, 'player_profile.html')
